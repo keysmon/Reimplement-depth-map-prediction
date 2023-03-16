8e29d077cad42e3daed5209e9a4c4b1f27a3dc2f
Dana <danabell841@gmail.com>
2023-03-16 00:55:13 -0700

    Update CNN architecture
    - use nn.Sequential for readability
    - add ReLU and dropout

--- network.py
+++ network.py
@@ -3,29 +3,34 @@ import torch.nn as nn
 class CoarseNetwork(nn.Module):
     def __init__(self):
         super(CoarseNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(3,96, 11, stride=4)
-        self.pool1 = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(3,256,5)
-        self.pool2 = nn.MaxPool2d(2,2)
-        self.conv3 = nn.Conv2d(3,384,3)
-        self.conv4 = nn.Conv2d(3,384,3)
-        self.conv5 = nn.Conv2d(3,384,3)
-        self.fc1 = nn.Linear(256,4096)
-        self.fc2 = nn.Linear(4096,4096)
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3),
+            nn.ReLU(),
+            
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3),
+            nn.ReLU(),
+
+            nn.Linear(in_features=256, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout2d(),
+            nn.Linear(in_features=4096, out_features=1)
+        )
+        
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
+        x = self.layers(x)
     
 image_class = CoarseNetwork()
 
-print(image_class.fc1)
+print(image_class)
 
