79e3286c653963838135fe740b53051ea5835c58
Hang Ruan <46987261+keysmon@users.noreply.github.com>
2023-04-02 18:47:32 -0700

    Add files via upload

--- neural_networks.py
+++ neural_networks.py
@@ -12,9 +12,9 @@ import numpy as np
 # (2284, 320, 240, 3)
 # (2284, 640, 480)
 # (2284, 640, 480)
-
 lambda_ = 0.5
 cutoff = 100
+
 def coarse_nn(X,Y):
     #print(X.shape)
     #print(Y.shape)
@@ -34,18 +34,55 @@ def coarse_nn(X,Y):
     model.add(keras.layers.Flatten())
     
     model.add(keras.layers.Dense(4096,activation='relu'))
-    model.add(keras.layers.Dropout(0.1))
+    model.add(keras.layers.Dropout(0.5))
 
     model.add(keras.layers.Dense(4800,activation='linear')) # output at 1/4 resolution of input
     model.add(keras.layers.Reshape((80,60)))
     
     model.compile(optimizer = tf.keras.optimizers.experimental.SGD(learning_rate=0.1),loss = Scale_invariant_loss,metrics = ['accuracy'],run_eagerly= False)
-    model.fit(x = X,y = Y, epochs = 10  ,batch_size = 32)
+    model.fit(x = X,y = Y, epochs = 5  ,batch_size = 32)
     #coarse_output = model.predict(X)
-    #model.save("my_model")
+    model.save("my_model")
     
     return coarse_output
     
+
+def coarse_nn(X,Y):
+    #print(X.shape)
+    #print(Y.shape)
+    63 x 80 x 60 + 80 x 60 = 64 x 80 x 60
+    # input was downsampled from the original by a factor of 2
+    input_1 = keras.layers.Input(shape=(320,240,3))
+    layer_1_1 = keras.layers.Conv2D(96,(11,11),strides = (4,4),activation= 'relu',input_shape =(320,240,1),padding='same')(input_1)
+    layer_1_2 = keras.layers.MaxPooling2D(pool_size = (2,2))(layer_1_1)
+    layer_1_3 = keras.layers.Conv2D(256,(5,5),activation= 'relu',padding='same')(layer_1_2)
+    layer_1_4 = keras.layers.MaxPooling2D(pool_size = (2,2))(layer_1_3)
+    layer_1_5 = keras.layers.Conv2D(384,(3,3),activation= 'relu',padding='same')(layer_1_4)
+    layer_1_6 = keras.layers.Conv2D(384,(3,3),activation= 'relu',padding='same')(layer_1_5)
+    layer_1_7 = keras.layers.Conv2D(384,(3,3),activation= 'relu',padding='same')(layer_1_6)
+    layer_1_8 = keras.layers.Flatten()(layer_1_7)
+    layer_1_9 = keras.layers.Dense(4096,activation='relu')(layer_1_8)
+    layer_1_10 = keras.layers.Dropout(0.1)(layer_1_9)
+    layer_1_11 = keras.layers.Dense(4800,activation='linear')(layer_1_10)
+    layer_1_12 = keras.layers.Reshape((80,60,1))(layer_1_11)
+    
+    
+    
+    layer_2_1 = keras.layers.Conv2D(63,(9,9),activation= 'relu',input_shape =(320,240,1),padding='same')(input_1)
+    layer_2_2 = keras.layers.MaxPooling2D(pool_size = (2,2),strides = 4)(layer_2_1)
+    layer_2_3 = keras.layers.Concatenate()([layer_1_12,layer_2_2])
+    
+    layer_2_4 = keras.layers.Conv2D(64,(5,5),activation= 'relu',padding='same')(layer_2_3)
+    layer_2_5 = keras.layers.Conv2D(1,(5,5),activation= 'linear',padding='same')(layer_2_4)
+    
+    
+    model = keras.Model(inputs = [input_1],outputs = [layer_2_5])
+    model.compile(optimizer = tf.keras.optimizers.experimental.SGD(learning_rate=0.01),loss = Scale_invariant_loss,metrics = ['accuracy'])
+    model.fit(x = X,y = Y, epochs = 5  ,batch_size = 10)
+    #coarse_output = model.predict(X)
+    model.save("my_model")
+    
+    return
 
 def coarse_nn_predict(input):
     model = keras.models.load_model("my_model", compile = False)
@@ -53,13 +90,12 @@ def coarse_nn_predict(input):
     coarse_output = tf.convert_to_tensor(coarse_output)
     return coarse_output
 
-    
+
 
 def local_nn(X,Y,coarse_output):
     input_2 = keras.layers.Input(shape=(320,240,3))
     layer2_1 = keras.layers.Conv2D(63,(9,9),activation= 'relu',input_shape =(320,240,1),padding='same')(input_2)
     layer2_2 = keras.layers.MaxPooling2D(pool_size = (2,2),strides = 4)(layer2_1)
-    
     
     layer2_3 = keras.layers.Concatenate()([coarse_output,layer2_2])
     
@@ -67,14 +103,15 @@ def local_nn(X,Y,coarse_output):
     layer2_5 = keras.layers.Conv2D(1,(5,5),activation= 'linear',padding='same')(layer2_5)
     model = keras.models.Model(input=[input_2],outputs = [layer2_5])
     model.compile(optimizer =  tf.keras.optimizers.experimental.SGD(learning_rate=0.1,momentum=0.9),loss = Scale_invariant_loss,metrics = ['accuracy'],run_eagerly= True)
-    model.fit(x = X,y = Y, epochs = 5 )
+    model.fit(x = X,y = Y, epochs = 5)
     
     return
 
 
 
 def Scale_invariant_loss(y_true, y_pred):
-    
+    #tf.print("y_true",y_true.shape)
+    #tf.print("y_pred",y_pred.shape)
     
     y_pred = tf.clip_by_value(y_pred,0,y_pred.dtype.max)
     
@@ -105,7 +142,7 @@ def main():
     coarse_output = coarse_nn(data_images,data_depth)
     #coarse_output = coarse_nn_predict(data_images)
     #local_nn(data_images,data_depth,coarse_output)
-    #test()
+    
     return
     
 if __name__ == "__main__":
